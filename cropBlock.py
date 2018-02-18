f181780d30b85a6b5ff95ca9e02227fbd74449d3
mnaufal75 <mnaufal75@gmail.com>
2018-02-18 17:54:09 +0700

    calculate BMComplexity

--- cropBlock.py
+++ cropBlock.py
@@ -1,5 +1,7 @@
 from PIL import Image
 
+THRESHOLD = 0.3
+
 
 def cropBlock(input, height=8, width=8):
     # im = Image.open(input)
@@ -18,7 +20,7 @@ def cropBlock(input, height=8, width=8):
     return cropList
 
 
-# output bitplane 8x24 list of bitplane 
+# output bitplane 8x24 list of bitplane
 def convertToBitplane(img, width=8, height=8):
     bitPlane = []
     for i in range(0, 24):
@@ -45,11 +47,38 @@ def convertToBitplane(img, width=8, height=8):
     return bitPlane
 
 
-# bitPlane 24x24 , width=8, height=8
-# def calculateBMComplexity(bitPlane, width=8, height=8):
-#     for plane in bitPlane:
+def stringToMatrix(ls, width=8, height=8):
+    y = [ls[i:i + height] for i in range(0, len(ls), width)]
+    return y
 
-#     return 0
+
+# bitPlane 24x24 , width=8, height=8
+# list of 24 complexity
+def calculateBMComplexity(bitPlane, width=8, height=8):
+    listComp = []
+    for plane in bitPlane:
+        comp = 0
+        pl = stringToMatrix(plane)
+        print(pl)
+        for i in range(0, 8):
+            for j in range(0, 8):
+                if j < 7:
+                    if (pl[i][j] != pl[i][j + 1]):
+                        comp += 1
+                if i < 7:
+                    if (pl[i][j] != pl[i + 1][j]):
+                        comp += 1
+                print(i, j, comp)
+        # for i in range(0, len(plane)):
+            # if (i + 1) < len(plane):
+            #     if (i + 1) % width > 0 and plane[i + 1] != plane[i]:
+            #         comp += 1
+            # if (i + 8) < len(plane):
+            #     if plane[i + 8] != plane[i]:
+            #         comp += 1
+            # print(i, comp)
+        listComp.append(comp / (width * (height - 1) + height * (width - 1)))
+    return listComp
 
 
 img = Image.open('C:/Users/mnaufal75/foto.png')
@@ -59,10 +88,12 @@ rgb_img = img.convert('RGB')
 cropImage = cropBlock(rgb_img, 200, 200)
 
 
-for iImage in cropImage:
-    for jImage in iImage:
-        bitPlane = convertToBitplane(jImage, 200, 200)
-        # print(bitPlane)
-        break
-    break
+# for iImage in cropImage:
+#     for jImage in iImage:
+#         bitPlane = convertToBitplane(jImage, 200, 200)
+#         # print(bitPlane)
+#         break
+#     break
+lists = ['1010101010101010101010101010101010101010101010101010101010101010']
+print(calculateBMComplexity(lists))
 # print(crop)
